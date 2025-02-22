7c32563195c91038d54f7523066d995273a3132d
Kasun_Udayanga <udayangakasun696@gmail.com>
2025-02-22 11:26:27 +0530

    feat: add About page and update Home component with navigation link

---
+++ app/about/page.js
@@ -0,0 +1,4 @@
+export default function About() {
+  return <h2>About is my next js first app </h2>;
+}
+
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <h2>Home</h2>
-    </>
+    <h2>Home is my next js first app </h2>
+    <Link href="/about">About</Link>
+      
+      </>
   );
 }
